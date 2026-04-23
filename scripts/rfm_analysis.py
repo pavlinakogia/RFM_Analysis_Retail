673539417f66ff464d38efda343c328a8ea25f8b
Pavlina Kogia <pavlina.kogia@gmail.com>
2026-04-23 23:06:42 +0300

    Update rfm_analysis.py

--- scripts/rfm_analysis.py
+++ scripts/rfm_analysis.py
@@ -2,22 +2,21 @@ import pandas as pd
 import datetime as dt
 import os
 
-# 1. Βρίσκουμε το σωστό μονοπάτι για το αρχείο Excel
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 file_path = os.path.join(base_path, 'data', 'online_retail.xlsx') #Δεν είναι πλέον ανεβασμένο γιατί ήταν πολύ μεγάλο αρχείο excel
 
-print("Φόρτωση του Excel... (Επειδή είναι μεγάλο θα πάρει περίπου 1-2 λεπτά)")
+print("Φόρτωση του Excel...")
 
 try:
-    # 2. Διαβάζουμε και τα δύο φύλλα από το ΕΝΑ αρχείο Excel που έχεις
+    # 2. Διαβάζουμε και τα δύο φύλλα από το ΕΝΑ αρχείο Excel 
     df_2009 = pd.read_excel(file_path, sheet_name='Year 2009-2010')
     df_2010 = pd.read_excel(file_path, sheet_name='Year 2010-2011')
 
     # 3. Τα ενώνουμε σε ένα μεγάλο σύνολο
     df = pd.concat([df_2009, df_2010], ignore_index=True)
-    print(f"✅ Τα δεδομένα φορτώθηκαν! Συνολικές γραμμές: {len(df)}")
+    print(f" Τα δεδομένα φορτώθηκαν! Συνολικές γραμμές: {len(df)}")
 
-    # 4. Καθαρισμός (όπως και πριν)
+    # 4. Καθαρισμός 
     df = df.dropna(subset=['Customer ID'])
     df = df[~df['Invoice'].astype(str).str.contains('C', na=False)]
     df = df[(df['Quantity'] > 0) & (df['Price'] > 0)]
@@ -34,12 +33,12 @@ try:
 
     rfm.columns = ['Recency', 'Frequency', 'Monetary']
 
-    # Αποθήκευση του αποτελέσματος σε CSV για να το έχουμε έτοιμο
+    # Αποθήκευση του αποτελέσματος σε CSV 
     output_path = os.path.join(base_path, 'data', 'rfm_output.csv')
     rfm.to_csv(output_path)
 
-    print(f"✅ Το αρχείο RFM δημιουργήθηκε: {output_path}")
+    print(f"Το αρχείο RFM δημιουργήθηκε: {output_path}")
     print(rfm.head())
 
 except Exception as e:
-    print(f"❌ Κάτι πήγε στραβά: {e}")
+    print(f"Κάτι πήγε στραβά: {e}")
